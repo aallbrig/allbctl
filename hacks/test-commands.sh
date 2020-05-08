36d5ceb389a7d5ab3643e8fd3b63b6f8102f6572
aallbright <aallbrig@gmail.com>
2020-05-08 10:08:27 -0400

    Test command script uses cleanup function

--- hacks/test-commands.sh
+++ hacks/test-commands.sh
@@ -2,34 +2,41 @@
 set -eo pipefail
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+function cleanup() {
+    popd
+    rm -rf tmp
+}
+
+trap cleanup EXIT
 
 mkdir -p tmp
 pushd "."
 pushd "tmp"
-trap "popd" EXIT
-    "${SCRIPT_DIR}"/../bin/allbctl --help
-    "${SCRIPT_DIR}"/../bin/allbctl
-    "${SCRIPT_DIR}"/../bin/allbctl completion
-    "${SCRIPT_DIR}"/../bin/allbctl completion zsh
-    "${SCRIPT_DIR}"/../bin/allbctl generate
-    "${SCRIPT_DIR}"/../bin/allbctl generate ansible
-    "${SCRIPT_DIR}"/../bin/allbctl generate ansible init
-    "${SCRIPT_DIR}"/../bin/allbctl generate ansible config
-    "${SCRIPT_DIR}"/../bin/allbctl generate ansible inventory
-    "${SCRIPT_DIR}"/../bin/allbctl generate ansible hostVar
-    "${SCRIPT_DIR}"/../bin/allbctl generate ansible groupVar
-    "${SCRIPT_DIR}"/../bin/allbctl generate ansible role --interactive=false
-    "${SCRIPT_DIR}"/../bin/allbctl generate dockerfile ansible
-    "${SCRIPT_DIR}"/../bin/allbctl generate git
-    "${SCRIPT_DIR}"/../bin/allbctl generate golang
-    "${SCRIPT_DIR}"/../bin/allbctl generate java
-    "${SCRIPT_DIR}"/../bin/allbctl generate kubernetes
-    "${SCRIPT_DIR}"/../bin/allbctl generate node
-    "${SCRIPT_DIR}"/../bin/allbctl generate python
-    "${SCRIPT_DIR}"/../bin/allbctl generate ruby
-    "${SCRIPT_DIR}"/../bin/allbctl generate scala
-    "${SCRIPT_DIR}"/../bin/allbctl generate shell
-    "${SCRIPT_DIR}"/../bin/allbctl youtube
-    "${SCRIPT_DIR}"/../bin/allbctl youtube list
-popd
-rm -rf tmp
+
+"${SCRIPT_DIR}"/../bin/allbctl --help
+"${SCRIPT_DIR}"/../bin/allbctl
+"${SCRIPT_DIR}"/../bin/allbctl completion
+"${SCRIPT_DIR}"/../bin/allbctl completion zsh
+"${SCRIPT_DIR}"/../bin/allbctl generate
+"${SCRIPT_DIR}"/../bin/allbctl generate ansible
+"${SCRIPT_DIR}"/../bin/allbctl generate ansible init
+"${SCRIPT_DIR}"/../bin/allbctl generate ansible config
+"${SCRIPT_DIR}"/../bin/allbctl generate ansible inventory
+"${SCRIPT_DIR}"/../bin/allbctl generate ansible hostVar
+"${SCRIPT_DIR}"/../bin/allbctl generate ansible groupVar
+"${SCRIPT_DIR}"/../bin/allbctl generate ansible role --interactive=false
+"${SCRIPT_DIR}"/../bin/allbctl generate dockerfile ansible
+"${SCRIPT_DIR}"/../bin/allbctl generate dockerfile alpine
+"${SCRIPT_DIR}"/../bin/allbctl generate git
+"${SCRIPT_DIR}"/../bin/allbctl generate golang
+"${SCRIPT_DIR}"/../bin/allbctl generate java
+"${SCRIPT_DIR}"/../bin/allbctl generate kubernetes
+"${SCRIPT_DIR}"/../bin/allbctl generate node
+"${SCRIPT_DIR}"/../bin/allbctl generate python
+"${SCRIPT_DIR}"/../bin/allbctl generate ruby
+"${SCRIPT_DIR}"/../bin/allbctl generate scala
+"${SCRIPT_DIR}"/../bin/allbctl generate shell
+"${SCRIPT_DIR}"/../bin/allbctl youtube
+"${SCRIPT_DIR}"/../bin/allbctl youtube list
+
+cleanup
