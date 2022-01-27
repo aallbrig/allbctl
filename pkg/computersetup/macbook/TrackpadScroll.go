d8004edebbee7ddef7c0ea7ffa04e4be1b9b2d8e
aallbright <aallbrig@gmail.com>
2022-01-27 11:53:34 -0500

    Natural scrolling enabled

--- pkg/computersetup/macbook/TrackpadScroll.go
+++ pkg/computersetup/macbook/TrackpadScroll.go
@@ -1,33 +1,66 @@
 package macbook
 
 type TrackpadScroll struct {
-	ConfigName    string
-	ExpectedState DefaultsCommand
+	ConfigName string
 }
 
 func (t TrackpadScroll) Name() string {
 	return t.ConfigName
 }
 
-var scrollingExpectedState = DefaultsCommand{
+var scrollingEnabled = DefaultsCommand{
 	Domain:        "com.apple.AppleMultitouchTrackpad",
 	Key:           "TrackpadScroll",
+	ExpectedValue: "1",
+	DefaultValue:  "1",
+}
+
+var scrollDirection = DefaultsCommand{
+	Domain:        "'Apple Global Domain'",
+	Key:           "com.apple.swipescrolldirection",
 	ExpectedValue: "0",
 	DefaultValue:  "1",
 }
 
 func NewTrackpadScrolling() *TrackpadScroll {
-	return &TrackpadScroll{ConfigName: "Trackpad Scroll", ExpectedState: scrollingExpectedState}
+	return &TrackpadScroll{ConfigName: "Trackpad Scroll"}
 }
 
 func (t TrackpadScroll) Validate() error {
-	return t.ExpectedState.Validate()
+	err := scrollDirection.Validate()
+	if err != nil {
+		return err
+	}
+
+	err = scrollingEnabled.Validate()
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 func (t TrackpadScroll) Install() error {
-	return t.ExpectedState.Install()
+	err := scrollDirection.Install()
+	if err != nil {
+		return err
+	}
+
+	err = scrollingEnabled.Uninstall()
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 func (t TrackpadScroll) Uninstall() error {
-	return t.ExpectedState.Uninstall()
+	err := scrollDirection.Uninstall()
+	if err != nil {
+		return err
+	}
+
+	err = scrollingEnabled.Uninstall()
+	if err != nil {
+		return err
+	}
+	return nil
 }
